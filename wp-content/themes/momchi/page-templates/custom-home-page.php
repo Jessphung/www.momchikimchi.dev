ee1c7091e88d2dd9e661f64a7ff0290a490be36a
Jessphung <jessicaphung@Jessicas-MacBook-Pro-3.local>
2016-02-19 20:14:17 -0800

    9th Push
    Home Page Edit

--- wp-content/themes/momchi/page-templates/custom-home-page.php
+++ wp-content/themes/momchi/page-templates/custom-home-page.php
@@ -9,37 +9,14 @@
 
 get_header(); ?>
 
-	<div class="flex-container">
-              <div class="flexslider">
-                <ul class="slides">
-                <?php $the_query = new WP_Query(array(
-				  'category_name' => 'featured', 'posts_per_page' => 3
-				  ));
-				?>
-    			  <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-                  <li>
-                    <?php the_post_thumbnail(); ?>
-                    <div class="caption_wrap">
-                    <div class="flex-caption">
-                    <div class="flex-caption-title">
-                    <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-                    </div>
-                    <p><?php echo striker_get_slider_excerpt(); ?>
-                    <a href="<?php the_permalink() ?>">...</a></p>
-                    </div>
-                    </div>
-                  </li>
-                <?php
-                    endwhile;
-                ?>
-                </ul>
-              </div>
-	</div>
+	
         
         
         <div id="primary_home" class="content-area">
 			<div id="content" class="fullwidth" role="main">
 
+                  <div class="row">
+   <div class="circleBase type3"> <img src="http://www.momchikimchi.dev/wp-content/uploads/2016/02/checheh_kimchi.jpg" alt="Kimchi"></div>
 
      <div class="featuretext">
 			 <h3><?php echo esc_attr( get_theme_mod( 'featured_textbox' ) ); ?></h3>
@@ -50,32 +27,7 @@ get_header(); ?>
 			</div>
 	</div>
     
-    <div class="section_thumbnails group">
-	<?php echo '<h3>' . __('Recent Posts', 'striker') . '</h3>'; ?>
-    
-  <?php $the_query = new WP_Query(array(
-  'showposts' => 4,
-  'post__not_in' => get_option("sticky_posts"),
-  ));
- ?>
-    <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-    <div class="col span_1_of_4">
-    <article class="recent">
-                
-				<?php
-			if ( has_post_thumbnail() ) {
-    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),'recent' );
-     echo '<img alt="post" class="imagerct" src="' . $image_src[0] . '">';
-}
-  			?>
-                 <div class="recent_title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-				
-    </article>
-    </div>	
-		
-	<?php endwhile; ?>
-
-    </div>
+   
 
 	<div class="section group">
     
